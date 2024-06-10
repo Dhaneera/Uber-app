bec33a3fc1b1d22e01271b026092f48aec1ce1e9
SDW-SWD <wickramsuriyashehan@gmail.com>
2024-06-10 15:29:05 +0530

    GoogleAutoComplete  function diploy to serach

--- Uber-App/app/HomeScreen.tsx
+++ Uber-App/app/HomeScreen.tsx
@@ -2,28 +2,67 @@ import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import NavOption from '@/components/NavOption'
 import { useNavigation } from 'expo-router'
-
-const HomeScreen:React.FC = () => {
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useDispatch } from 'react-redux';
+import { setdestination, setOrgin } from '@/features/navSlice';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+const HomeScreen: React.FC = () => {
   const navigation = useNavigation()
+  const dispatch = useDispatch()
 
-      
   useLayoutEffect(() => {
     navigation.setOptions({
-        headerShown: false,
+      headerShown: false,
     })
-}, [navigation])
-  
+  }, [navigation])
+
   return (
     <SafeAreaView className='h-full'>
-     <View className='p-5'>
-      <Image
-      source={{
-        uri: 'https://links.papareact.com/gzs',
-      }}style={{width:100,height:100,resizeMode:'contain'}} 
-      />
-     <NavOption/>
+      <View className='p-5'>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }} style={{ width: 100, height: 100, resizeMode: 'contain' }} />
 
-     </View>
+        <GooglePlacesAutocomplete
+          placeholder='from'
+          styles={{
+            container: {
+              flex: 0,
+              width: 350,
+            },
+            textInput: {
+              fontSize: 18,
+              fontWeight: 800,
+            }
+          }}
+          onPress={(data, detils) => {
+            dispatch(setOrgin({
+              location: {
+                lat: detils?.geometry.location.lat,
+                lng: detils?.geometry.location.lng
+
+              },
+              description: data.description
+            }))
+            dispatch(setdestination(null))
+          }}
+          fetchDetails={true}
+          // returnKeyType={'search'}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+
+
+        />
+        <NavOption />
+
+      </View>
     </SafeAreaView>
   )
 }
