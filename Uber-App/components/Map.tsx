80f6d6e05b730f0389d27d21cd2750217077e0cf
SDW-SWD <wickramsuriyashehan@gmail.com>
2024-06-10 15:25:15 +0530

    enviroment variab le key construct to save google map api key !!!

--- Uber-App/components/Map.tsx
+++ Uber-App/components/Map.tsx
@@ -1,11 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { selectOrgin } from '@/features/navSlice'
+import { useSelector } from 'react-redux'
 
-const Map = () => {
+const Map: React.FC = () => {
+
+  const orgin = useSelector(selectOrgin)
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+      <MapView className='flex-1' mapType='mutedStandard'
+          initialRegion={{
+              latitude: orgin?.location.lat,
+              longitude: orgin?.location.lng,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+          }}>
+          {orgin?.location.lat && orgin?.location.lng && (
+              <Marker coordinate={{
+                  latitude: orgin.location.lat,
+                  longitude: orgin.location.lng
+              }}
+                  title={orgin?.description}
+                  identifier='orgin'
+              />)}
+      </MapView>
+
   )
 }
 
